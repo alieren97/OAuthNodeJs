b86294dbf875f750a58808ccb98f05b802f9a618
alieren97 <alierensh3@gmail.com>
2021-04-02 00:42:27 +0300

    Log out Added

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -1,12 +1,15 @@
 const router = require('express').Router()
 const passport = require('passport')
+
+
 //auth login
 router.get('/login',(req,res)=>{
     res.render('login',{user:req.user})
 })
 
 router.get('/logout',(req,res)=>{
-    res.send('You logged out');
+    req.logout()
+    res.redirect('/')
 })
 
 //auth with google
